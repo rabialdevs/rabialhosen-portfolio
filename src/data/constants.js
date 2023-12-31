64e83003350478f9e84d216158fb59edba90e807
RABIAL HOSEN <rabialhosen404@gmail.com>
2023-12-31 20:05:12 +0530

    Update resume link

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/rabialdevs",
-  resume: "https://flowcv.com/resume/iif7k4jns4",
+  resume: "https://drive.google.com/file/d/1o-3JtG2iyzGrOsPlqTmfHe9WTEGvD2Qi/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rabialhosen/",
   twitter: "",
 };
